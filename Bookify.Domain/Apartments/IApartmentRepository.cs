b62e2776d04a87ddcf8d92e08eaba06824cbed00
Кирилл Зинин <5pacejamm@mail.ru>
2026-05-27 23:06:26 +0300

    Domain layer implementation

---
+++ Bookify.Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public interface IApartmentRepository
+{
+    Task<Apartment?> GetByIdAsync( Guid apartmentId, CancellationToken cancellationToken = default );
+}
---
+++ Bookify.Domain/Reviews/ReviewErrors.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews;
+
+public static class ReviewErrors
+{
+    public static Error NotEligable = new(
+        "Review.NotEligable",
+        "The review is not eligable because booking is not completed yet" );
+}
