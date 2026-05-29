eaa2c43b8e4f78c67bb5df8cefddb17873fd5c06
Кирилл Зинин <5pacejamm@mail.ru>
2026-05-29 22:42:02 +0300

    UseCases and some abstractions

---
+++ Bookify.Application/Bookings/GetBooking/BookingResponse.cs
@@ -0,0 +1,37 @@
+﻿using Bookify.Domain.Bookings;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+public class BookingResponse
+{
+    public Guid Id { get; init; }
+
+    public Guid ApartmentId { get; init; }
+
+    public Guid UserId { get; init; }
+
+    public int Status { get; init; }
+
+    public decimal PriceAmount { get; init; }
+
+    public string PriceCurrency { get; init; }
+
+    public decimal CleaningFeeAmount { get; init; }
+
+    public string CleaningFeeAmountCurrency { get; init; }
+
+    public decimal AmenitiesUpChargeAmount { get; init; }
+
+    public string AmenitiesUpChargeAmountCurrency { get; init; }
+
+    public decimal TotalPriceAmount { get; init; }
+
+    public string TotalPriceCurrency { get; init; }
+
+    public DateOnly DurationStart { get; init; }
+
+    public DateOnly DurationEnd { get; init; }
+
+    public DateTime ConfirmedOnUtc { get; init; }
+}
---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetBookingQueryHandler( ISqlConnectionFactory connectionFactory )
+    {
+        _sqlConnectionFactory = connectionFactory;
+    }
+
+    public async Task<Result<BookingResponse>> Handle( GetBookingQuery request, CancellationToken cancellationToken )
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string sql = """
+            SELECT
+                id as Id,
+                apartment_id as ApartmentId,
+                user_id as UserId,
+                status as Status,
+                price_for_period_amount as PriceForPeriodAmount,
+                price_for_period_currency as PriceForPeriodCurrency,
+                cleaning_fee_amount as CleaningFeeAmount,
+                cleaning_fee_amount_currency as CleaningFeeAmountCurrency,
+                amenities_up_charge_amount as AmenitiesUpChargeAmount,
+                amenities_up_charge_currency as AmenitiesUpChargeCurrency,
+                total_price_amount as TotalPriceAmount,
+                total_price_amount_currency as TotalPriceAmountCurrency,
+                duration_start as DurationStart,
+                duration_end as DurationEnd,
+                created_on_utc as CreatedOnUtc
+            FROM bookings
+            WHERE id = @BookingId
+            """;
+
+        var booking = await connection.QueryFirstOrDefaultAsync<BookingResponse>(
+            sql,
+            new
+            {
+                request.BookingId
+            } );
+
+        return booking;
+    }
+}
